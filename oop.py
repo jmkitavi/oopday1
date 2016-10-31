d24b4dbcc4f6bdb925affdfbeffcd4fa538e1077
sirjmkitavi <kitavimjoseph@gmail.com>
2016-10-31 17:01:55 +0300

    added use cases

--- oop.py
+++ oop.py
@@ -1,16 +1,28 @@
 class User(object):
 	access_level=1
 	name=''
-	def __init__ (self,name):
+	def __init__ (self, name):
 		self.name =  name
-	def can_access(level):
-		return access_level > level
+	def can_access(self, level):
+		return self.access_level >= level
 
 class Employee(User):
-	def can_access(level):
-		return (access_level> level or level =3)
+	access_level = 2
+	def can_access(self, level):
+		return self.access_level >= level or level == 3
 
 class Manager(Employee):
+	access_level =3
+	def can_access(self, level):
+		return (self.access_level >= level or level in [2,6])
 
-	def can_access(level):
-		return (access_level> level or (level in [2,3]))
+
+gian = User("Gian")
+adrin = Employee("Adrin")
+ian = Manager("Limo")
+users = [gian,adrin,ian]
+
+for level in range(1,7):
+	for u in users:
+		print u.name + " can access. level " + str(level) + ' ' + str(u.can_access(level))
+	print "\n"
