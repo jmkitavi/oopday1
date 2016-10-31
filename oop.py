0f894ff1156ba5a600a3510e7e9cc0491195f6df
sirjmkitavi <kitavimjoseph@gmail.com>
2016-10-31 16:38:36 +0300

    fixed typo, and added manager

--- oop.py
+++ oop.py
@@ -3,11 +3,14 @@ class User(object):
 	name=''
 	def __init__ (self,name):
 		self.name =  name
-	def canAccess(level):
+	def can_access(level):
 		return access_level > level
 
 class Employee(User):
 	def can_access(level):
 		return (access_level> level or level =3)
 
+class Manager(Employee):
 
+	def can_access(level):
+		return (access_level> level or (level in [2,3]))
